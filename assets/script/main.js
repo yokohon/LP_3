ad7b90cc511536eb019882d8d999d71bcf53f539
honda.youko <kyogranatus@jcom.home.ne.jpp>
2025-02-09 22:10:06 +0900

    footer

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,20 +1,17 @@
-$('.carousel').slick({
-    autoplay: false, // 自動再生
-    autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
-    dots: true, // ドットを表示
-    fade: true, // フェードで切り替え
-    mobileFirst: true, //初期設定がモバイル
-    slidesToShow: 1,
-    slidesToScroll: 1,
+// $('.carousel').slick({
+//     autoplay: false, 
+//     autoplaySpeed: 1000, 
+//     dots: true, 
+//     fade: true, 
+//     mobileFirst: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+// });
 
-    responsive: [
-        {
-            breakpoint: 768, // 768px以上の画面サイズ
-            settings: {
-                slidesToShow: 3, // このサイズでの表示スライド数
-                slidesToScroll: 3,
-                fade: false, // フェードを無効にする
-            }
-        }
-    ]
-});
+
+$('.carousel').slick({
+    centerMode: true,
+    centerPadding: '10%',
+    slidesToShow: 1,
+    dots: true, 
+  });
