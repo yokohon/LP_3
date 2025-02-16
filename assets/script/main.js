c83dfd4052e7c769d4db04cc512bbcab5af1550d
honda.youko <kyogranatus@jcom.home.ne.jpp>
2025-02-16 18:26:49 +0900

    pc slick 画像表示

--- assets/script/main.js
+++ assets/script/main.js
@@ -10,8 +10,21 @@
 
 
 $('.carousel').slick({
-    centerMode: true,
-    centerPadding: '10%',
-    slidesToShow: 1,
-    dots: true, 
-  });
+  centerMode: true,
+  centerPadding: '10%',
+  slidesToShow: 1,
+  dots: true,
+
+  responsive: [
+    {
+      breakpoint: 768, // 768px以上の画面サイズ
+      settings: {
+        centerMode: false,
+        centerPadding: false,
+        slidesToShow: 3, // このサイズでの表示スライド数
+        dots: false,
+      }
+    }
+  ]
+
+});
