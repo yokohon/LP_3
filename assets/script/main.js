73249b5d506f64a270e776aaf2af0948b45a5bcd
honda.youko <kyogranatus@jcom.home.ne.jpp>
2025-02-17 22:18:10 +0900

    pc slick

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,19 +1,10 @@
-// $('.carousel').slick({
-//     autoplay: false, 
-//     autoplaySpeed: 1000, 
-//     dots: true, 
-//     fade: true, 
-//     mobileFirst: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-// });
-
 
 $('.carousel').slick({
   centerMode: true,
   centerPadding: '10%',
   slidesToShow: 1,
   dots: true,
+  mobileFirst: true, //初期設定がモバイル
 
   responsive: [
     {
@@ -22,7 +13,7 @@ $('.carousel').slick({
         centerMode: false,
         centerPadding: false,
         slidesToShow: 3, // このサイズでの表示スライド数
-        dots: false,
+        // dots: false,
       }
     }
   ]
