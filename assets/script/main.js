33ddfafbb0ee044c72904ffff85d5a6db42f957e
yokohon <kyogranatus@jcom.home.ne.jp>
2025-08-18 22:39:12 +0900

    html　やり直し2

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,21 +1,21 @@
 
 $('.carousel').slick({
-  centerMode: true,
-  centerPadding: '8.333%',
-  slidesToShow: 1,
-  dots: true,
-  mobileFirst: true, //初期設定がモバイル
-  arrows: false,
+    centerMode: true,
+    centerPadding: '8.333%',
+    slidesToShow: 1,
+    dots: true,
+    mobileFirst: true, //初期設定がモバイル
+    arrows: false,
 
-  responsive: [
-    {
-      breakpoint: 768, // 768px以上の画面サイズ
-      settings: {
-        centerPadding: '0',
-        slidesToShow: 3, // このサイズでの表示スライド数
-        // dots: false,
-      }
-    }
-  ]
+    responsive: [
+        {
+            breakpoint: 768, // 768px以上の画面サイズ
+            settings: {
+                centerPadding: '0',
+                slidesToShow: 3, // このサイズでの表示スライド数
+                // dots: false,
+            }
+        }
+    ]
 
 });
